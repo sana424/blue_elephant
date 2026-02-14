4fb7b7fd0594ead1363fcdb86aa36709dfdd942a
sanha <leah2414@gmail.com>
2026-02-14 15:02:06 +0900

    공유2

--- asset/js/blue.js
+++ asset/js/blue.js
@@ -1,6 +1,7 @@
 const blue = document.querySelector("#blue");
 const blueBottomTxt = document.querySelector("#blue .bottom_text");
-const bgVideoBlue = document.querySelector('#videoLogo .bg_video_blue');
+const bgVideoBlue = document.querySelector("#videoLogo .bg_video_blue");
+const circle = document.querySelector(".circles .circle.center");
 
 let BTWidth = blueBottomTxt.getBoundingClientRect().width;
 
@@ -18,12 +19,57 @@ ScrollTrigger.create({
   },
 });
 
-gsap.to(bgVideoBlue,{
-  scale : 10,
-  scrollTrigger : {
-    trigger : blue,
-    start : `top+=${1080}px top`,
-    end : `top+=${2800}px top`,
-    scrub : true  
-  }
-})
+gsap.to(bgVideoBlue, {
+  width: '1000vw',
+  height: '1000vh',
+  scrollTrigger: {
+    trigger: blue,
+    start: `top+=${1080}px top`,
+    end: `top+=${2800}px top`,
+    scrub: true,
+    onLeave: () => {
+      bgVideoBlue.classList.add("hidden");
+    },
+    onEnterBack: () => {
+      bgVideoBlue.classList.remove("hidden");
+    },
+    onEnter: () => {
+      bgVideoBlue.classList.remove("hidden");
+    }
+  },
+});
+
+gsap.to(circle, {
+  width: 1500,
+  height: 1500,
+  scrollTrigger: {
+    trigger: blue,
+    start: `top+=${2400}px top`,
+    end: `top+=${3000}px top`,
+    scrub: true,
+  },
+});
+
+ScrollTrigger.create({
+  trigger: blue,
+  start: `top+=${2800}px top`,
+  onEnter: () => {
+    document.querySelector(".circles .circle.center").classList.add("active");
+    // document.querySelector(".circles .circle.left").classList.add("active");
+    // document.querySelector(".circles .circle.right").classList.add("active");
+  },
+  onEnterBack: () => {
+    document
+      .querySelector(".circles .circle.center")
+      .classList.remove("active");
+    // document.querySelector(".circles .circle.left").classList.remove("active");
+    // document.querySelector(".circles .circle.right").classList.remove("active");
+  },
+  onLeaveBack: () => {
+    document
+      .querySelector(".circles .circle.center")
+      .classList.remove("active");
+    // document.querySelector(".circles .circle.lefrt").centersList.remove("active");
+    // document.querySelector(".circles .circle.right").classList.remove("active");
+  },
+});
